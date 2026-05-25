80d016476f26884b6a6a97c7657a419b32645c11
nande324 <nande324@users.noreply.github.com>
2026-05-25 21:34:51 +0800

    chore(admin): 路由配置支持开发环境
    - 开发环境使用根路径，生产环境使用 /admin/

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory("/admin/"),
+  history: createWebHistory(process.env.NODE_ENV === "production" ? "/admin/" : "/"),
   routes: [
     { path: "/", redirect: "/statistics" },
     {
